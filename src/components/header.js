ca5f8953f3ee37f9283afa58ffdf298be6f192cb
Tolu Olubode <toluwanimiolubode@gmail.com>
2018-10-08 13:38:39 -0400

    fixed header and wave issue

--- src/components/header.js
+++ src/components/header.js
@@ -8,10 +8,7 @@ const Header = ({ siteTitle }) => (
     <Link to="/"><img width="200" src={require('../images/spotify-jamroom.svg')} /></Link>
   </div>
   <div className="HeaderGroup">
-    <Link to="/courses">Premium</Link>
-    <Link to="/downloads">Join the jam</Link>
-    <Link to="/workshops">Log in</Link>
-    <Link to="/buy"><button>Sign up</button></Link>
+    <a href="https://tolu.design/#/spotify-jam-room/"  target="_blank"><button>Learn more</button></a>
   </div>
 </div>
 )
