188fbf351499d5a2a75f946d45e3032e0ddd945a
Tolu Olubode <toluwanimiolubode@gmail.com>
2018-10-23 13:20:16 -0400

    metatags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import MetaTags from 'react-meta-tags';
 import icon16 from '../images/favicon-16x16.png'
 
 
@@ -9,6 +10,11 @@ import './index.css'
 
 const Layout = ({ children, data }) => (
   <div>
+    <MetaTags>
+      <meta name="description" content="Spotify Jamroom: A new way to share music with loved ones" />
+      <meta property="og:title" content="Spotify Jamroom" />
+      <meta property="og:image" content="../images/spotify-jamroom.svg" />
+    </MetaTags>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
